1c3682a1935a4729656044ad1eecad465873fe2a
dronovs <od270891dss@gmail.com>
2021-08-18 08:49:38 +0300

    init

---
+++ script/script.js
@@ -0,0 +1,5 @@
+alert('Hello');
+let userName = prompt('What is your name?');
+let userBornYear = 2021 - +prompt('How old are you?');
+
+alert(`Nice to meet you, ${userName}! You were born in ${userBornYear}`);
